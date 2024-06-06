bec46b60fa541b82650f0a3f7c727527f598cccb
ld <13821891726@163.com>
2024-06-06 15:02:25 +0800

    [junw] 目录结构调整

---
+++ jLab-2/jLab-2-1/src/main/java/my/junw/jlab2/JLab2Application.java
@@ -0,0 +1,13 @@
+package my.junw.jlab2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JLab2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JLab2Application.class, args);
+    }
+
+}
---
+++ jLab-2/jLab-2-1/src/test/java/my/junw/jlab2/JLab2ApplicationTests.java
@@ -0,0 +1,19 @@
+package my.junw.jlab2;
+
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import my.junw.jlab2.demo.Person;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Slf4j
+class JLab2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+
+}
